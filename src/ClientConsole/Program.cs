10d90d9241945d3bddf103b6597469c0a343cc7f
Harrison Uhunghama <harrison.uhunghama@igniteinsurancesystems.onmicrosoft.com>
2022-07-07 11:39:57 +0100

    Adding client project to demo our messages

---
+++ src/ClientConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security;
+using System.Threading;
+using log4net;
+using opc.ua.pubsub.dotnet.client;
+using opc.ua.pubsub.dotnet.client.common;
+using opc.ua.pubsub.dotnet.client.common.Settings;
+using Client = opc.ua.pubsub.dotnet.client.Client;
+namespace TestClient
+{
+    class Program
+    {
+        private static readonly ILog Logger = LogManager.GetLogger( typeof( Program ) );
+
+
+
+        static void Main( string[] args )
+        {
+            Log4Net.Init();
+
+            var setting = new Settings();
+            setting.Client = new opc.ua.pubsub.dotnet.client.common.Settings.Client();
+            setting.Client.BrokerHostname = "localhost";
+            setting.Client.DefaultSubscribeTopicName = "#";
+            setting.Client.RawDataMode = false;
+
+            var client = new Client( setting, "MyClient" );
+
+            client.Connect();
+
+            // Wait for connection
+            while ( !client.IsConnected )
+            {
+                Logger.Info( "Waiting for Client to Connect" );
+                Thread.Sleep( 1000 );
+            }
+
+            client.Subscribe( "#" );
+
+            // Wait for end
+            while ( true )
+            {
+                Thread.Sleep( 1000 );
+            }
+        }
+    }
+}
---
+++ src/SSToFDemoPublisher/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.IO;
+
+namespace SSoTFDemoPublisher
+{
+    class Program
+    {
+        static void Main( String[] args )
+        {
+            string UserName = "a8000User";
+
+            string Password = "SiemensM";
+
+            string HostName = "localhost";
+
+
+
+            //Main entry point to the RabbitMQ .NET AMQP client
+
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+
+            {
+
+                UserName = UserName,
+
+                Password = Password,
+
+                HostName = HostName
+
+            };
+
+
+            var connection = connectionFactory.CreateConnection();
+
+            var model = connection.CreateModel();
+
+            var properties = model.CreateBasicProperties();
+            properties.Persistent = false;
+
+
+            string projectDirectory = Directory.GetParent( Environment.CurrentDirectory ).Parent.FullName;
+            string messagedump = $"{projectDirectory}\\messagedump";
+            foreach ( FileInfo f in new DirectoryInfo( messagedump ).GetFiles( "*.txt" ) )
+            {
+                string mycontent = File.ReadAllText( f.FullName );
+                byte[] payload = Convert.FromBase64String( mycontent );
+                model.BasicPublish( "amq.topic", "directexchange_key", properties, payload );
+                Console.WriteLine( "Message Sent" );
+            }
+
+
+        }
+    }
+}
